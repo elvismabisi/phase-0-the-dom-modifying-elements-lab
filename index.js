3c7099296bb308b41191b958cb5e8cf68ab43375
Gregory <elvismabisi@gmail.com>
2022-12-06 12:46:32 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+main.remove()
+
+const newHeader = document.createElement("h1")
+
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "Greg is the champion";
